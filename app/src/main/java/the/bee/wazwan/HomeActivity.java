a7847d3a4e7db67a542fbfa82b57202eeba3f390
asifbu <asibul@asibul.com>
2022-03-30 00:13:52 +0600

    first commit

---
+++ app/src/main/java/the/bee/wazwan/HomeActivity.java
@@ -0,0 +1,27 @@
+package the.bee.wazwan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class HomeActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        textView = findViewById(R.id.mydata);
+
+
+    }
+}
